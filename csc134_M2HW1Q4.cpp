9a26e6d784fc985383fa3b80c35c4090411a9663
sccsc1342025 <sebastiancam711@gmail.com>
2025-02-22 22:22:24 +0000

    csc134_M2HW1Q4.cpp

---
+++ csc134_M2HW1Q4.cpp
@@ -0,0 +1,26 @@
+// CSC 134
+// M2HW1-GOLD
+// Sebastian Camacho
+// 02/22/2025
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "question 4" << endl;
+    
+    // variables
+    string school = "FTCC";
+    string team = "Trojans";
+
+    // cheering message
+    cout << "Let's go " << school << endl;
+    cout << "Let's go " << school << endl;
+    cout << "Let's go " << school << endl;
+    cout << "Let's go " << team << endl;
+
+    return 0;
+}
