e0df10bfbf6a542d0ba737b7dac62da037aca4ff
sccsc1342025 <sebastiancam711@gmail.com>
2025-02-22 22:15:14 +0000

    csc134_M2HW1Q3.cpp

--- csc134_M2HW1Q3.cpp
+++ csc134_M2HW1Q3.cpp
@@ -9,12 +9,35 @@ using namespace std;
 
 int main()
 {
+    cout << "question 3" << endl;
+    
     // variables
     int numPizzas, slicesPerPizza, numVisitors;
     int totalSlices, slicesNeeded, leftoverSlices;
 
     // number of pizza ordered
-    
+    cout << "How many pizzas will you order ";
+    cin >> numPizzas;
+
+    // slices per pizza
+    cout << "How many slices per pizza ";
+    cin >> slicesPerPizza;
+
+    // how many visitors
+    cout << "How many visitors will come ";
+    cin >> numVisitors;
+
+    // calculations
+    totalSlices = numPizzas * slicesPerPizza;
+    slicesNeeded = numVisitors * 2;
+    leftoverSlices = totalSlices - slicesNeeded;
+
+    cout << "Total slices of pizza " << totalSlices << endl;
+    cout << "Slices needed for visitors " << slicesNeeded << endl;
+
+    if (leftoverSlices >= 0) {
+        cout << "Leftover slices of pizza: " << leftoverSlices << endl;
+    }
 
     return 0;
 }
